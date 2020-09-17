0819705d93a0c4a142b036ed4e0c11a06628acfa
dhilgart <55857612+dhilgart@users.noreply.github.com>
2020-09-17 15:08:06 -0500

    Added placeholder functions in clietn_run.py

--- client_run.py
+++ client_run.py
@@ -5,5 +5,31 @@ Dan Hilgart <dhilgart@gmail.com>
 
 import TWIML_codenames
 import TWIML_codenames_API_Client
-import my_model
+import my_model as model
+import asyncio
 
+async def check_status_loop():
+    """
+    The main loop that creates a new task every X seconds to find out whether anything is expected from the player
+    """
+    while True:
+        loop.create_task(check_status())
+        await asyncio.sleep(60)
+
+async def check_status():
+    """
+    Asks the server what the current status is for this player.
+    """
+    await status = TWIML_codenames_API_Client.check_status(loop)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(check_status_loop())
+
+    try:
+        loop.run_until_complete(task)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        loop.close()
