3d31d2dbfe4abe508b6fd4caa23a327ec155509e
dhilgart <55857612+dhilgart@users.noreply.github.com>
2020-09-17 12:03:24 -0500

    added body class definitions to API_Server

--- TWIML_codenames_API_Server.py
+++ TWIML_codenames_API_Server.py
@@ -4,7 +4,15 @@ Dan Hilgart <dhilgart@gmail.com>
 """
 
 from fastapi import FastAPI, Response
+from pydantic import BaseModel
 import pickle
+
+class generate_clues_body(BaseModel):
+    clue_word: str
+    clue_count: int
+
+class generate_guesses_body(BaseModel):
+    guesses: list
 
 root="/"
 app = FastAPI()
@@ -12,19 +20,19 @@ app = FastAPI()
 def send_as_bytes(var_to_send):
     return Response(content=pickle.dumps(var_to_send))
 
-@app.get(root+"generate_clue/")
-def send_generate_clue_info():
+@app.get(root+"{game_id}/generate_clue")
+def send_generate_clue_info(game_id: int, user_id: int, user_key: int):
     return
 
-@app.post(root+"generate_clue/")
-def receive_generate_clue_info():
+@app.post(root+"{game_id}/generate_clue")
+def receive_generate_clue_info(game_id: int, user_id: int, user_key: int, data: generate_clues_body):
     return
 
-@app.get(root+"generate_guesses/")
-def send_generate_guesses_info():
+@app.get(root+"{game_id}/generate_guesses")
+def send_generate_guesses_info(game_id: int, user_id: int, user_key: int):
     return
 
-@app.post(root+"generate_guesses/")
-def receive_generate_guesses_info():
+@app.post(root+"{game_id}/generate_guesses")
+def receive_generate_guesses_info(game_id: int, user_id: int, user_key: int, data: generate_guesses_body):
     return
 
