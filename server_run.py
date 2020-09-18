05ead55faf0da0f14ea4f5c155fead804401a952
dhilgart <55857612+dhilgart@users.noreply.github.com>
2020-09-17 19:41:02 -0500

    renamed TWIML_codenames_API_Server to server_run

---
+++ server_run.py
@@ -0,0 +1,45 @@
+"""
+server_run.py: runs the server for TWIMLfest 2020 codenames competition
+Dan Hilgart <dhilgart@gmail.com>
+"""
+
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+import pickle
+
+class generate_clues_body(BaseModel):
+    clue_word: str
+    clue_count: int
+
+class generate_guesses_body(BaseModel):
+    guesses: list
+
+root="/"
+app = FastAPI()
+
+def send_as_bytes(var_to_send):
+    return Response(content=pickle.dumps(var_to_send))
+
+@app.get(root)
+def get_player_status(user_id: int, user_key: int):
+    return
+
+@app.get(root+"{game_id}/generate_clue/")
+def send_generate_clue_info(game_id: int, user_id: int, user_key: int):
+    return
+
+@app.post(root+"{game_id}/generate_clue/")
+def receive_generate_clue_info(game_id: int, user_id: int, user_key: int, data: generate_clues_body):
+    clue_word=data.clue_word
+    clue_count=data.clue_count
+    return
+
+@app.get(root+"{game_id}/generate_guesses/")
+def send_generate_guesses_info(game_id: int, user_id: int, user_key: int):
+    return
+
+@app.post(root+"{game_id}/generate_guesses/")
+def receive_generate_guesses_info(game_id: int, user_id: int, user_key: int, data: generate_guesses_body):
+    guesses = data.guesses
+    return
+
