5474d105252fb71eacc46644475a7f48f08a5beb
nup0013 <nidhin.pattaniyil@walmart.com>
2020-10-05 17:20:41 -0400

    remove requirement.txt move models.py

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.8
 WORKDIR /app
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install numpy==1.18.* nltk==3.5.* fastapi[all]==0.61.* python-dotenv==0.14.* pymongo==3.11.* 
 
 COPY . .
 CMD python server_run.py
--- router/user.py
+++ router/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 import config
 import pymongo 
-import models
+from server import models
 import logging
 
 
@@ -27,8 +27,8 @@ async def update(user_stats:models.UserStats):
     #user = UserStats(user_name=user_name, score=score, num_games=num_games)
         
 
-    filer = {'user_name': user_stats.user_name}
-    res:pymongo.results.UpdateResult = db['user'].replace_one(filer ,user_stats.dict() , upsert= True)
+    query_filter = {'user_name': user_stats.user_name}
+    res:pymongo.results.UpdateResult = db['user'].replace_one(query_filter ,user_stats.dict() , upsert= True)
     
     logging.debug(f"Updated {user_stats.user_name} with {user_stats}")
     return res.acknowledged
